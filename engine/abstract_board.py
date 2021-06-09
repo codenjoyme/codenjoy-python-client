e9cb4f31e932a2d2646080d74c24d282d0c28912
Roman Kulyniak <roman_kulyniak@epam.com>
2021-06-09 14:43:51 +0300

    [clients][python] implement bomberman game

---
+++ engine/abstract_board.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python3
+
+###
+# #%L
+# Codenjoy - it's a dojo-like platform from developers to developers.
+# %%
+# Copyright (C) 2018 Codenjoy
+# %%
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.html>.
+# #L%
+###
+
+from math import sqrt
+from engine.point import Point
+from games.bomberman.element import Element
+
+
+class AbstractBoard:
+    BLAST_RANGE = 3
+
+    def __init__(self, board_string):
+        self._string = board_string.replace('\n', '')
+        self._len = len(self._string)
+        self._size = int(sqrt(self._len))
+
+    def _find_all(self, element):
+        _points = []
+        _a_char = element.get_char()
+        for i, c in enumerate(self._string):
+            if c == _a_char:
+                _points.append(self._strpos2pt(i))
+        return _points
+
+    def get_at(self, x, y):
+        return Element(self._string[self._xy2strpos(x, y)])
+
+    def is_at(self, x, y, element_object):
+        return element_object == self.get_at(x, y)
+
+    def is_near(self, x, y, elem):
+        _is_near = False
+        if not Point(x, y).is_bad(self._size):
+            _is_near = (self.is_at(x + 1, y, elem) or
+                        self.is_at(x - 1, y, elem) or
+                        self.is_at(x, 1 + y, elem) or
+                        self.is_at(x, 1 - y, elem))
+        return _is_near
+
+    def count_near(self, x, y, elem):
+        _near_count = 0
+        if not Point(x, y).is_bad(self._size):
+            for _x, _y in ((x + 1, y), (x - 1, y), (x, 1 + y), (x, 1 - y)):
+                if self.is_at(_x, _y, elem):
+                    _near_count += 1
+        return _near_count
+
+    def _strpos2pt(self, strpos):
+        return Point(*self._strpos2xy(strpos))
+
+    def _strpos2xy(self, strpos):
+        return (strpos % self._size, strpos // self._size)
+
+    def _xy2strpos(self, x, y):
+        return self._size * y + x
+
+
+if __name__ == '__main__':
+    raise RuntimeError("This module is not designed to be ran from CLI")
---
+++ games/bomberman/solver.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+###
+# #%L
+# Codenjoy - it's a dojo-like platform from developers to developers.
+# %%
+# Copyright (C) 2018 Codenjoy
+# %%
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public
+# License along with this program.  If not, see
+# <http://www.gnu.org/licenses/gpl-3.0.html>.
+# #L%
+###
+
+from engine.direction import Direction
+from games.bomberman.board import Board
+
+
+class BombermanSolver:
+
+    def __init__(self):
+        self._direction = None
+        self._board = None
+        self._last = None
+        self._count = 0
+
+    def get(self, board_string):
+        self._board = Board(board_string)
+        print("Board \n{}".format(self._board.to_string()))
+        _command = self.next_step()
+        print("Answer: {}".format(_command))
+        print("-------------------------------------------------------------")
+
+        return _command
+
+    def next_step(self):
+        # make your action
+        return Direction('ACT').to_string()
+
+
+if __name__ == '__main__':
+    raise RuntimeError("This module is not intended to be ran from CLI")
