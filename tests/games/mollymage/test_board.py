0f0d0f2e3d07c58e85d4ad2a853a95d0a5312add
Roman_Kulyniak <roman_kulyniak@epam.com>
2021-08-10 12:59:07 +0300

    [clients][python][mollymage] add test for a new perk

--- tests/games/mollymage/test_board.py
+++ tests/games/mollymage/test_board.py
@@ -78,9 +78,9 @@ class TestBoard(TestCase):
         self.assertEquals([Point(2, 0)], board.find_blasts())
 
     def test_find_perks(self):
-        board = Board("#cr" + "#i+" + "###")
+        board = Board("#cr" + "#i+" + "#T#")
         self.assertEquals(
-            [Point(1, 1), Point(1, 2), Point(2, 1), Point(2, 2)],
+            [Point(1, 0), Point(1, 1), Point(1, 2), Point(2, 1), Point(2, 2)],
             board.find_perks())
 
     def test_report(self):
