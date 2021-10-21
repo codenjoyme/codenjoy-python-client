afbf984bd46d8ed8a3dbb8b208ebb48fbc31876a
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-10-21 21:21:56 +0300

    [clients][all] Added /Q to all remove stuff command.

--- 1-download-env.bat
+++ 1-download-env.bat
@@ -12,7 +12,7 @@ if "%INSTALL_LOCALLY%"=="" ( goto :skip )
 
 cd %ROOT%
 IF EXIST %TOOLS%\python.zip (
-    del %TOOLS%\python.zip
+    del /Q %TOOLS%\python.zip
 )
 powershell -command "& { set-executionpolicy remotesigned -s currentuser; [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48; $client=New-Object System.Net.WebClient; $client.Headers['User-Agent']='PoweShell script';  $client.DownloadFile('%ARCH_PYTHON%','%TOOLS%\python.zip') }"
 rd /S /Q %TOOLS%\..\.python
