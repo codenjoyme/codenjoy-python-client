39f15252dd4c6a2f0f90eb4ed3985278fe323801
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-19 19:09:54 +0200

    [clients][all][bat] Reformat.

--- 0-settings.bat
+++ 0-settings.bat
@@ -7,7 +7,7 @@ if "%BOARD_URL%"==""   ( set BOARD_URL=http://127.0.0.1:8080/codenjoy-contest/bo
 
 set ROOT=%CD%
 
-if "%SKIP_TESTS%"=="" ( set SKIP_TESTS=true)
+if "%SKIP_TESTS%"==""  ( set SKIP_TESTS=true)
 
 set CODE_PAGE=65001
 chcp %CODE_PAGE%
@@ -16,12 +16,14 @@ set TOOLS=%ROOT%\.tools
 set ARCH=%TOOLS%\7z\7za.exe
 
 rem Set to true if you want to ignore python installation on the system
-if "%INSTALL_LOCALLY%"=="" ( set INSTALL_LOCALLY=true)
+if "%INSTALL_LOCALLY%"==""     ( set INSTALL_LOCALLY=true)
 
 if "%INSTALL_LOCALLY%"=="true" ( set PYTHON_HOME=)
-if "%PYTHON_HOME%"=="" ( set NO_PYTHON=true)
-if "%NO_PYTHON%"=="true" ( set PYTHON_HOME=%ROOT%\.python)
-if "%NO_PYTHON%"=="true" ( set PATH=%PYTHON_HOME%;%PATH%)
+
+if "%PYTHON_HOME%"==""         ( set NO_PYTHON=true)
+if "%NO_PYTHON%"=="true"       ( set PYTHON_HOME=%ROOT%\.python)
+if "%NO_PYTHON%"=="true"       ( set PATH=%PYTHON_HOME%;%PATH%)
+
 set PYTHON=%PYTHON_HOME%\python
 
 echo off
