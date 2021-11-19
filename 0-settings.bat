8aab66cf51f7a8a83e8901d3dee4d79c62d80b92
Oleksandr Baglai <oleksandr_baglai@epam.com>
2021-11-19 23:47:17 +0200

    [clients][python][bat] Refactoring. Added PATH.

--- 0-settings.bat
+++ 0-settings.bat
@@ -3,7 +3,7 @@ call lib :color Setup variables...
 echo on
 
 if "%GAME_TO_RUN%"=="" ( set GAME_TO_RUN=mollymage)
-if "%BOARD_URL%"==""   ( set BOARD_URL=http://127.0.0.1:8080/codenjoy-contest/board/player/0?code=000000000000)
+if "%BOARD_URL%"==""   ( set BOARD_URL=https://dojorena.io/codenjoy-contest/board/player/dojorena5?code=953820862434373766)
 
 set ROOT=%CD%
 
@@ -20,17 +20,18 @@ if "%INSTALL_LOCALLY%"==""     ( set INSTALL_LOCALLY=true)
 
 if "%INSTALL_LOCALLY%"=="true" ( set PYTHON_HOME=)
 
-if "%PYTHON_HOME%"==""         ( set NO_PYTHON=true)
-if "%NO_PYTHON%"=="true"       ( set PYTHON_HOME=%ROOT%\.python)
-if "%NO_PYTHON%"=="true"       ( set PATH=%PYTHON_HOME%;%PATH%)
+if "%PYTHON_HOME%"==""   ( set NO_PYTHON=true)
+if "%NO_PYTHON%"=="true" ( set PYTHON_HOME=%ROOT%\.python)
+if "%NO_PYTHON%"=="true" ( set PATH=%PYTHON_HOME%;%PATH%)
 
 set PYTHON=%PYTHON_HOME%\python
 
 echo off
+call lib :color PATH=%PATH%
 call lib :color PYTHON_HOME=%PYTHON_HOME%
 echo on
 
-set ARCH_URL=https://www.python.org/ftp/python/3.9.6/python-3.9.6-embed-amd64.zip
+set ARCH_URL=https://www.python.org/ftp/python/3.9.9/python-3.9.9-embed-amd64.zip
 set ARCH_FOLDER=
 
 set PYTHON_CLIENT_HOME=%ROOT%
