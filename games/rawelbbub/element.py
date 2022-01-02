c8c56f874fcf718f0b232a3c3422c0ea6ca14ec3
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-03 00:56:51 +0200

    [rawelbbub][clients][all] Refactoring. Rename BAND -> EXPLOSION.

--- games/rawelbbub/element.py
+++ games/rawelbbub/element.py
@@ -32,9 +32,9 @@ elements = dict(
 
     REEFS='☼',
 
-        # A destroyed hero. A new appears just in a second.
+        # Explosion site. It disappears in a second.
 
-    BANG='Ѡ',
+    EXPLOSION='Ѡ',
 
         # Ice, having driven onto which hero will begin to drift.
         # During a skid, hero will repeat the old commands for several
