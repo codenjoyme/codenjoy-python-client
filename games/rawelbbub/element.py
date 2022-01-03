c2b0d14bc19ec5ad34f50045cbc5c9b5a5f5c8c0
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-03 19:17:36 +0200

    [balancer][balancer-frontend][rawelbbub][clients][all] Refactoring. Rename BULLET -> TORPEDOE. Fixed balancer back/front readme and game settings.

--- games/rawelbbub/element.py
+++ games/rawelbbub/element.py
@@ -138,9 +138,12 @@ elements = dict(
 
     ICEBERG_DESTROYED=' ',
 
-        # Completely destroyed wall. Wall will recover over time.
+        # Torpedo - is a self-propelled underwater missile designed to
+        # be fired from a submarine and to explode on reaching a
+        # target. The target can be an iceberg, another submarine and
+        # other elements under water.
 
-    BULLET='•',
+    TORPEDO='•',
 
         # Your hero is pointing up.
 
